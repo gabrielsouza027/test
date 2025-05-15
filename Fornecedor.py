ff4b7375abd09282128d7ca30be62970a0d324b4
Gabriel Souza <89928513+gabrielsouza027@users.noreply.github.com>
2025-05-15 11:45:52 -0300

    Update Fornecedor.py

--- Fornecedor.py
+++ Fornecedor.py
@@ -24,7 +24,7 @@ def get_all_data_from_supabase():
         try:
             all_data = []
             page_size = 1000
-            offset = 0
+            offset = 1000000000000000
 
             while True:
                 response = (
