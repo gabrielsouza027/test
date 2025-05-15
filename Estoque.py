660785cf3192549e899b4b828113c2ca060a7dad
Gabriel Souza <89928513+gabrielsouza027@users.noreply.github.com>
2025-05-15 16:01:52 -0300

    Update Estoque.py

--- Estoque.py
+++ Estoque.py
@@ -81,8 +81,8 @@ def fetch_supabase_data(_cache, table, columns_expected, date_column=None, last_
 
     try:
         all_data = []
-        limit = 1
-        max_pages = 3000
+        limit = 1000
+        max_pages = 10
         filters = []
 
         if last_update and date_column:
